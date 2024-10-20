9076974a5fb4515c0e543a1f4c3bf222233b4dde
Łukasz Faruga <lukasz.faruga1@gmail.com>
2024-10-20 23:01:36 +0200

    added logs and consistent, reproducible generator

--- src/load_balancer/minimal_example.py
+++ src/load_balancer/minimal_example.py
@@ -1,20 +1,22 @@
 import sys
-import asyncio
 import random
+import asyncio
 import numpy as np
 from loguru import logger
 from typing import AsyncGenerator
+from datetime import datetime
+from typing import OrderedDict
 
 
 def log_format(record):
     level = record["level"].name.rjust(4)
     source = record["extra"].get("source", "???")
-    return f"{record['time']:YYYY-MM-DD HH:mm:ss} | {level} | [{source}] {record['message']}\n"
+    time = record["extra"].get("ts") or f"{record['time']:HH:mm:ss.SSS}"
+    return f"{time} | {level} | [{source}] {record['message']}\n"
 
 
 logger.remove()
-logger.add(
-    sys.stderr, format=log_format, level="INFO")
+logger.add(sys.stderr, format=log_format, level="INFO")
 
 
 type Request = int
@@ -23,21 +25,34 @@ type RequestGenerator = callable[[], AsyncGenerator[Request, None]]
 
 
 class Server:
-    def __init__(self, name: str, buffer_size: int, mu: float) -> None:
+    def __init__(self, name: str, buffer_size: int, mu: float, start_time: datetime) -> None:
         self.name = name
         self.queue = asyncio.Queue(maxsize=buffer_size)
         self.mean_processing_time = mu
+        self.start_time = start_time
 
+        self.history = []
         self.processed = 0
         self.rejected = 0
+
+    def ts(self):
+        return (datetime.now() - self.start_time + datetime.min).strftime("%H:%M:%S:%f")[:-3]
 
     async def receive(self, request: Request) -> None:
         if self.queue.full():
             self.rejected += 1
+
+            data = OrderedDict(ts=self.ts(), source=self.name,
+                               request=request, status='rejected')
+            self.history.append(data)
             logger.info(f"❌ Rejected  request {
-                        request:>3} (queue full)", source=self.name)
+                        request:>3} (queue full)", **data)
             return
+        data = OrderedDict(ts=self.ts(), source=self.name,
+                           request=request, status='queued')
+        self.history.append(data)
         logger.debug(f"Queueing request {request}", source=self.name)
+
         await self.queue.put(request)
 
     async def simulate_processing_request(self) -> float:
@@ -53,8 +68,11 @@ class Server:
             self.processed += 1
             self.queue.task_done()
 
+            data = OrderedDict(ts=self.ts(), source=self.name,
+                               request=request, status='processed')
+            self.history.append(data)
             logger.info((f"✅ Processed request {request:>3} "
-                         f"({processing_time:.2f}s)"), source=self.name)
+                         f"({processing_time:.2f}s)"), **data)
 
 
 def route_random(servers: list[Server]) -> Server:
@@ -66,15 +84,21 @@ def route_shortest_queue(servers: list[Server]) -> Server:
 
 
 def create_requests_generator_poisson(lambda_: float) -> AsyncGenerator[Request, None]:
+    cache = []
+
     async def generator():
+        cache_ = iter(cache)
         request = 0
         while True:
             logger.debug(f"Created request {request}", source="GEN")
             yield request
             request += 1
 
-            wait_time = np.random.exponential(1/lambda_)
+            if not (wait_time := next(cache_, None)):
+                wait_time = np.random.exponential(1/lambda_)
+                cache.append(wait_time)
             await asyncio.sleep(wait_time)
+
     return generator
 
 
@@ -100,7 +124,8 @@ async def simulate(
 ) -> None:
     print(f"\nStart symulacji - polityka {routing_fn.__name__}")
 
-    servers = [Server(f"WS{i+1}", server_buffer_size, server_mu)
+    start_time = datetime.now()
+    servers = [Server(f"WS{i+1}", server_buffer_size, server_mu, start_time)
                for i in range(num_servers)]
 
     processes = [
@@ -127,6 +152,12 @@ async def simulate(
     # TODO: to jest do implementacji
     # print(f"Średnia długość kolejki: {avg_queue_length}")
 
+    logs = [log
+            for server in servers
+            for log in server.history]
+    print("Logi:")
+    print(logs)
+
 
 async def main():
     params = dict(
@@ -139,6 +170,7 @@ async def main():
 
     await simulate(**params, routing_fn=route_random)
     await simulate(**params, routing_fn=route_shortest_queue)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
