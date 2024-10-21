4e9da3b97ab615c17a2bf0c958a76f8bfa3ecd4b
BartoszBartt <bartosz.bartoszewski21@gmail.com>
2024-10-21 22:01:26 +0200

    front end end end

--- src/load_balancer/visual_functions.py
+++ src/load_balancer/visual_functions.py
@@ -50,62 +50,74 @@ def process_queue_data(file_path):
 
 
 
-def plot_buffer_levels(data_df, rejects, max_buffer):
+def plot_buffer_levels(data_df, rejects, max_buffer,plot=True):
+
     # Get unique sources from the DataFrame
     unique_sources = data_df['source'].unique()
-    
+    avg_queues_p = {}
     # Iterate through each source to create the plots
     for source in unique_sources:
-        # Filter the data for the current source
+        temp_sum = 0
+        df_a = data_df[data_df['source'] == source]
+        for idx in range(df_a.shape[0]-1):
+            temp_sum += (df_a['ts_in_ms'].values[idx+1] - df_a['ts_in_ms'].values[idx]) * df_a['currently_in_queue'].values[idx]
+        avg_queue_p = np.round(temp_sum / df_a['ts_in_ms'].values[-1] / max_buffer *100,2)
+        avg_queues_p[f'{source}'] =avg_queue_p
+        
         source_data = data_df[data_df['source'] == source]
-        
-        # Create a new figure for each source
-        plt.figure(figsize=(12, 8))
-        
-        # Step plot for currently in queue
-        plt.step(source_data['ts_in_ms'], source_data['currently_in_queue'], where='mid', alpha=0)
-        plt.fill_between(source_data['ts_in_ms'], source_data['currently_in_queue'], step='mid')
+        if plot:
+            plt.figure(figsize=(12, 8))
+            
+            plt.step(source_data['ts_in_ms'], source_data['currently_in_queue'], where='mid', alpha=0)
+            plt.fill_between(source_data['ts_in_ms'], source_data['currently_in_queue'], step='mid')
 
-        # Set the limits for the y-axis
-        plt.ylim(0, max_buffer*1.2)
-        
-        # Scatter plot for rejected requests
-        plt.scatter(rejects['ts_in_ms'], np.ones(len(rejects)) * max_buffer, 
-                    color='red', s=20, label='Rejected Requests')
+            plt.ylim(0, max_buffer*1.2)
+            
+            plt.scatter(rejects['ts_in_ms'], np.ones(len(rejects)) * max_buffer, 
+                        color='red', s=20, label='Rejected Requests')
 
-        # Set title and labels
-        plt.title(f'Buffer Levels for {source}')
-        plt.xlabel('Time')
-        plt.ylabel('Currently in Queue')
-        plt.legend()
-        
-        # Set x-axis ticks
-        plt.xticks([np.min(source_data['ts_in_ms']), np.max(source_data['ts_in_ms'])])
-        
-        # Show the plot
-        plt.tight_layout()
-        st.pyplot(plt)
+            plt.title(f'Buffer Levels for {source}, \nAVG queue fillage - {avg_queue_p}%')
+            plt.xlabel('Time')
+            plt.ylabel('Currently in Queue')
+            plt.legend()
+            
+            # Set x-axis ticks
+            plt.xticks([np.min(source_data['ts_in_ms']), np.max(source_data['ts_in_ms'])])
+            
+            # Show the plot
+            plt.tight_layout()
+            st.pyplot(plt)
+        else:
+            return avg_queues_p
 
-
-
-def plot_processing_times(merged_df):
+def plot_processing_times(merged_df, plot = True):
     unique_sources = merged_df['source_queued'].unique()
-    num_sources = len(unique_sources)
+    result = merged_df.copy()
+    avg_times = {}
+    stds_times = {}
+    for source in unique_sources:
+        avg_times[f'{source}'] = result[result['source_queued']==source]['processing_time_ms'].mean()
+        stds_times[f'{source}'] = result[result['source_queued']==source]['processing_time_ms'].std()
+    avg_times['Global'] = result['processing_time_ms'].mean()
+    stds_times['Global'] = result[result['source_queued']==source]['processing_time_ms'].std()
+   
+    if plot:
+        num_sources = len(unique_sources)
+        cmap = cm.get_cmap('tab20', num_sources)  
+        color_mapping = {source: cmap(i) for i, source in enumerate(unique_sources)}
 
-    cmap = cm.get_cmap('tab20', num_sources)  
-    color_mapping = {source: cmap(i) for i, source in enumerate(unique_sources)}
-
-    colors = merged_df['source_queued'].map(color_mapping)
+        colors = merged_df['source_queued'].map(color_mapping)
     
+        fig = plt.figure(figsize=[12, 8])
+        plt.bar(merged_df['request'], merged_df['processing_time_ms'], color=colors)
 
-    fig = plt.figure(figsize=[12, 8])
-    plt.bar(merged_df['request'], merged_df['processing_time_ms'], color=colors)
+        plt.xlabel('Request ID')
+        plt.ylabel('Processing Time (ms)')
+        plt.title('Processing Time per Request')
+        
 
-    plt.xlabel('Request ID')
-    plt.ylabel('Processing Time (ms)')
-    plt.title('Processing Time per Request')
-    
-
-    handles = [plt.Rectangle((0, 0), 1, 1, color=color_mapping[source]) for source in unique_sources]
-    plt.legend(handles, unique_sources, title="Source")
-    st.pyplot(fig)
+        handles = [plt.Rectangle((0, 0), 1, 1, color=color_mapping[source]) for source in unique_sources]
+        plt.legend(handles, unique_sources, title="Source")
+        st.pyplot(plt)
+    else:
+        return avg_times, stds_times
